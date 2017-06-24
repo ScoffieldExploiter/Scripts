c75b94592a8f794ca42a0793bad5cbd55318dc3b
Scoth <scoffieldexploiter@gmail.com>
2017-06-24 16:35:00 -0300

    Update caesarbf.py

--- caesarbf.py
+++ caesarbf.py
@@ -1,20 +1,19 @@
 # Script criado para fins de estudos...
-alpha_lower = [x for x in "abcdefghijklmnopqrstuvwxyz"]
-alpha_upper = [x for x in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-msg_enc = raw_input("Digite a mensagem cifrada: ")
-for key in range(0,26):
-	result = ""
-	for x in msg_enc:
-		if x.isupper():
-			if x in alpha_upper:
-				result += alpha_upper[alpha_upper.index(x)-key]
-		else:
-			if x in alpha_lower:
-				result += alpha_lower[alpha_lower.index(x)-key]
-			else:
-				result += x
-	if key < 9:
-		print "Tentativa %s  = " %(key+1),result
-	else: 
-		print "Tentativa %s = " %(key+1),result
-print "\nBrute force finalizado\n"
+alpha_lower = [x for x in "abcdefghijklmnopqrstuvwxyz"] # Alfabeto de letrar minusculas ['a','b','c',...,'z']
+alpha_upper = [x for x in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"] # Alfabeto de letras maiusculas ['A','B','C',...,'Z']
+msg_enc = raw_input("Digite a mensagem cifrada: ") # Recebe a mensagem cifrada
+for key in range(0,26): # Para cada chave possivel
+	result = "" # Resultado final
+	for x in msg_enc: # Para cada letra da mensagem cifrada
+		if x.isupper(): # se a letra estiver em maiuscula
+			result += alpha_upper[alpha_upper.index(x)-key] # adicione ao resultado a letra que estiver na posição da letra atual - a chave
+		else: # se não
+			if x in alpha_lower: # se a letra estiver em minuscula
+				result += alpha_lower[alpha_lower.index(x)-key] # adicione ao resultado a letra que estiver na posição da letra atual - a chave
+			else: # caso seja outro tipo de caracter
+				result += x # adicione o caracter, sem nenhuma mudança
+	if key < 9: # só p embelezar o output
+		print "Tentativa %s  = " %(key+1),result # só p embelezar o output
+	else:  # s p embelezar o output
+		print "Tentativa %s = " %(key+1),result # só p embelezar o output
+print "\nBrute force finalizado\n" # FIM
